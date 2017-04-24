f9d8a4d91bfa5437dd76d6af0ff5ec081002e6d5
Your Name <dicomitis13@gmail.com>
2017-04-24 15:17:07 -0600

    added button(non functional) and backgrounds to home page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
+      <div className='homelanding-page'>
+        <div className='homepage-title'>
+          <h1> Game Name </h1>
+          <p> Come join the adventure of a lifetime! </p>
+          <button className='play-button' type='button'>PLAY NOW!</button>
+        </div>
       </div>
     );
   }
--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -1,8 +1,14 @@
-import React from 'react';
-
-const HomePage = (props) =>
-  <div className='homelanding-page'>
-    <h1> Game Name </h1>
-  </div>
-
-  export default HomePage;
+// import React from 'react';
+//
+// const HomePage = (props) =>
+//   <div className='homelanding-page'>
+//     <div className=''>
+//       <h1> Game Name </h1>
+//       <p> Come join the adventure of a lifetime! </p>
+//     </div>
+//     <div>
+//       <button className='play-button' type='button'>PLAY NOW!</button>
+//     </div>
+//   </div>
+//
+//   export default HomePage;
