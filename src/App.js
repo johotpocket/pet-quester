7bc6f7a18a6fa316fb68fbe5ba1b61838cd584d6
Your Name <dicomitis13@gmail.com>
2017-04-24 14:46:26 -0600

    added background to home page and h1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import HomePage from './Containers/HomePage';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>PET QUESTER</h2>
-        </div>
-        <p className="App-intro">
-          Nothing here yet
-        </p>
+        <HomePage />
       </div>
     );
   }
---
+++ src/Containers/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const HomePage = (props) =>
+  <div className='homelanding-page'>
+    <h1> Game Name </h1>
+  </div>
+
+  export default HomePage;
