adb67caee18fd10d3480db2e4e1463ef465455b9
Your Name <dicomitis13@gmail.com>
2017-04-27 20:55:02 -0600

    added api routes ffor monsters and checked endpoints in postman

---
+++ controllers/api/monsters.js
@@ -0,0 +1,36 @@
+const Monster = require('../../models/Monsters');
+
+exports.all = (req, res) => {
+  Monster.find((err, data) => {
+    if (!data) return res.status(404).send("no Monsters found");
+    if (err) return res.status(500).send(err, "error finding all monsters");
+    res.json(data);
+  });
+};
+
+exports.create = (req, res) => {
+  let newMonster = new Monster();
+  newMonster.name = req.body.name;
+  newMonster.species = req.body.species;
+  newMonster.weakness = req.body.weakness;
+
+  console.log(newMonster, "NEW MONSTER WAS SAVED")
+
+  newMonster.save((err, data) => {
+    if(err){
+      res.send(err);
+    } else {
+      res.json({ data: data, message: "Monster successfully added!"})
+    }
+  })
+}
+
+exports.getOne = (req, res) => {
+  Monster.findById(req.params.monster_id, (err, data) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.json(data);
+    }
+  })
+}
--- controllers/api/routes.js
+++ controllers/api/routes.js
@@ -1,5 +1,6 @@
 const World = require('./worlds');
 const Choice = require('./choices')
+const Monster = require('./monsters')
 
 module.exports = (app) => {
   app.post('/api/worlds', World.create);
@@ -8,4 +9,7 @@ module.exports = (app) => {
   app.post('/api/choices', Choice.create);
   app.get('/api/choices', Choice.all);
   app.get('/api/choices/:choice_id', Choice.getOne);
+  app.post('/api/monsters', Monster.create);
+  app.get('./api/monsters', Monster.all);
+  app.get('/api/monsters/:monster_id', Monster.getOne);
 };
