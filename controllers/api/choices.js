1ad58233dde7bd0e005ce4b4aafe30675686acfc
Your Name <dicomitis13@gmail.com>
2017-04-27 19:53:37 -0600

    added route for choice model, tested in postman

---
+++ controllers/api/choices.js
@@ -0,0 +1,34 @@
+const Choice = require('../../models/Choices');
+
+exports.all = (req, res) => {
+  Choice.find((err, data) => {
+    if (!data) return res.status(404).send("no worlds found");
+    if (err) return res.status(500).send(err, "error finding all worlds");
+    res.json(data);
+  });
+};
+
+exports.create = (req, res) => {
+  let newChoice = new Choice();
+  newChoice.text = req.body.text;
+
+  console.log(newChoice, "NEW CHOICE WAS SAVED");
+
+  newChoice.save((err, data) => {
+    if(err){
+      res.send(err);
+    } else {
+      res.json({ data: data, message: "Choice successfully added!"})
+    }
+  })
+}
+
+exports.getOne = (req, res) => {
+  Choice.findById(req.params.choice_id, (err, data) => {
+    if(err) {
+      res.send(err);
+    } else {
+      res.json(data);
+    }
+  })
+}
--- controllers/api/routes.js
+++ controllers/api/routes.js
@@ -1,7 +1,11 @@
 const World = require('./worlds');
+const Choice = require('./choices')
 
 module.exports = (app) => {
   app.post('/api/worlds', World.create);
   app.get('/api/worlds', World.all);
   app.get('/api/worlds/:world_id', World.getOne);
+  app.post('/api/choices', Choice.create);
+  app.get('/api/choices', Choice.all);
+  app.get('/api/choices/:choice_id', Choice.getOne);
 };
