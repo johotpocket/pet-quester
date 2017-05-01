f767faecf105af6075eb6105c3dd5c57f00d3972
johotpocket <jo.hotpocket@gmail.com>
2017-05-01 09:27:07 -0600

    started story page

--- client/src/Story.js
+++ client/src/Story.js
@@ -4,7 +4,14 @@ class Story extends Component {
   render() {
     return (
       <div>
-        <h1> Test Story Page </h1>
+        <h1> Your story begins here </h1>
+        <p> No memories of before, no memory of who you are... you awake with only the clothes on your back
+        and a vague sense of unease. As your eyes focus to the dim candle-lit light, you take in your surroundings.
+        A comfy but simple and old looking couch is underneath you, and stone walls surround you in a small room.
+        A large oak door is opposite to you.
+        Sunlight streamsthrough a small open window, and small birds can be heard chirping away. You glimpse the tops of
+        trees outside, too.
+        </p>
       </div>
     );
   }
