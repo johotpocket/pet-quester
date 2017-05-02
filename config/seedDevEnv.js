6821ac027cb5ccfc9de57ce650796ae6a3700643
johotpocket <jo.hotpocket@gmail.com>
2017-05-02 10:30:37 -0600

    added images to scenes

--- config/seedDevEnv.js
+++ config/seedDevEnv.js
@@ -13,11 +13,13 @@ exports.seed = () => {
   var sceneStartingRoom = new Scene ({ startingScene: true,
                             typeOfScene: "normal",
                             description: "a dimly lit room with stone walls",
+                            image: "http://img07.deviantart.net/2a70/i/2011/158/a/2/a_room_in_a_castle_by_ravenslane-d3iadga.jpg",
                             choices: [choicesStartingRoomLeave._id] })
   sceneStartingRoom.save()
   var sceneCastleHall = new Scene ({ startingScene: false,
                             typeOfScene: "normal",
                             description: "a stone hall lined with knight armor and portraits of frowning nobles",
+                            image: "https://cdnb.artstation.com/p/assets/images/images/001/786/803/large/tina-popadic-zs-castle-hall.jpg?1452720318",
                             choices: [choicesForestExplore._id, choicesCastleExplore._id] })
   sceneCastleHall.save()
 
@@ -38,11 +40,13 @@ exports.seed = () => {
   var sceneForestStart = new Scene ({ startingScene: false,
                             typeOfScene: "normal",
                             description: "the edge of a typical fairytale forest",
+                            image: "http://weknownyourdreamz.com/images/forest/forest-04.jpg",
                             choices: [choicesReturnToCastle._id, choicesClimbLadder._id] })
   sceneForestStart.save()
   var sceneTreeWalkway = new Scene ({ startingScene: false,
                             typeOfScene: "normal",
                             description: "a walkway high above the forest floor. vicious bats block your path",
+                            image: "http://redwoods.co.nz/wordpress/wp-content/uploads/2015/12/IMG_0188compressed.jpg",
                             choices: [choicesFightBats._id, choicesFleeFromBats._id] })
   sceneTreeWalkway.save()
 
