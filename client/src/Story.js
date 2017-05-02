3a939ecb28b035818f03dced8996a3f8d7a0f4d3
Your Name <dicomitis13@gmail.com>
2017-05-02 17:23:20 -0600

    started styling home snd story page

--- client/src/Story.js
+++ client/src/Story.js
@@ -5,7 +5,9 @@ class Story extends Component {
   render() {
     return (
       <div>
-        <h1> Your story begins here </h1>
+        <div>
+          <h1 className='story-title'> Your story begins here </h1>
+        </div>
         <p> No memories of before, no memory of who you are... you awake with only the clothes on your back
         and a vague sense of unease. As your eyes focus to the dim candle-lit light, you take in your surroundings.
         A comfy but simple and old looking couch is underneath you, and stone walls surround you in a small room.
