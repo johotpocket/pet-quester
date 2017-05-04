1da9171123251172f5db3cccab77ca543652198a
Your Name <dicomitis13@gmail.com>
2017-05-04 11:50:31 -0600

    styled home and story pages. added instructions to story page

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -13,8 +13,16 @@ class HomePage extends Component {
           <Link to="/story" className='play-button' type='button'>PLAY NOW!</Link>
         </div>
         <div className='home-about'>
-          <h2>Welcome!</h2>
-          <p> About? </p>
+          <h2>About!</h2>
+          <p> Hi there! Welcome to our game! Pet Quester was made by johotpocket and tdicomitis
+          (github profiles, if you want to check us out). This game was made during our 12
+          week full stack web development bootcamp(shoutout to Big Sky Code Academy) for
+          our group project. The game play is very simple, a link style game that takes you
+          through different adventures and prompts you to make choices along the way. The
+          idea for our game was originally to do a text adventure game, but when we started
+          researching a little more, we found some cool link style games and were both intrigued
+          by the idea of a link base game. It was important to create something we were both excited
+          about. Hope you enjoy the game as much as we did building it!</p>
         </div>
       </div>
     )
