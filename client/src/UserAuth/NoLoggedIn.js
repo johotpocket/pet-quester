34fd5fd65ff88e1934beb3f1f6ff345b64defa01
Your Name <dicomitis13@gmail.com>
2017-05-04 13:59:05 -0600

    added signup link to our login page

--- client/src/UserAuth/NoLoggedIn.js
+++ client/src/UserAuth/NoLoggedIn.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const NoLoggedIn = () =>
-  <div>
-    <h4> You are not logged in. Bummer </h4>
+  <div className ='nologgedin-page'>
+    <h4> You are not logged in. Bummer. Signup or login to continue! </h4>
     <Link className="btn button-alarm" to="/login">Log in here!</Link>
+    <Link className="btn button-alarm" to="/signup">Sign up here!</Link>
   </div>
 
 export default NoLoggedIn;
